a903c59863263cca7f2c573fc69f04eee3532866
Rok Bohinc <rok.bohinc86@gmail.com>
2024-05-21 17:08:05 +0200

    add export of labels to the piepline

--- DatasetGeneration/MergeMetricsAndVideos.py
+++ DatasetGeneration/MergeMetricsAndVideos.py
@@ -1,4 +1,5 @@
 # %%
+import csv
 import os
 import pandas as pd
 import numpy as np
@@ -59,17 +60,13 @@ merged = pd.merge_asof(
 data_7i = merged[merged["Club Type"] == "7 Iron"]
 
 # Define columns of interest for missed shots
-columns_of_interest = ["Apex Height", "Carry Distance"]
-
-# Get summary statistics for filtering thresholds
-summary = data_7i[columns_of_interest].describe()
+filters = [("Apex Height", 10),("Carry Distance", 90)]
 
 # Filter out missed shots
-for column in columns_of_interest:
-    data_7i = data_7i[data_7i[column] > float(summary[column]["25%"])]
+for column, value in filters:
+    data_7i = data_7i[data_7i[column] > value]
 
-# Add shot_number in order to make sure the videos and the lables are refferencing the same shot
-# Adding row number column starting from 1
+# Defining the export path for each video
 data_7i["shot_number"] = range(1, len(data_7i) + 1)
 export_path = "/Users/rokbohinc/Documents/Work/Golf_AI/Golfshot_Categoriser/data/extracted/videos/"
 data_7i["export_path"] = export_path + data_7i["shot_number"].astype(str) + ".mp4"
@@ -79,4 +76,9 @@ if __name__ == "__main__":
     video_paths = list(zip(data_7i["videoPath"], data_7i["export_path"]))
     modelpath = "/Users/rokbohinc/Documents/Work/Golf_AI/Golfshot_Categoriser/DatasetGeneration/SwingNet/models/swingnet_2000.pth.tar"
     export_SwingNet_videos(video_paths=video_paths, modelpath=modelpath)
+# %%
+
+label_path = "/Users/rokbohinc/Documents/Work/Golf_AI/Golfshot_Categoriser/data/extracted/labels/labels.csv"
+data_7i[["shot_type"]].to_csv(label_path, index=False, sep=",", mode="w")
+
 # %%
